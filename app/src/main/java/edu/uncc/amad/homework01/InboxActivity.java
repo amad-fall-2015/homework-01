6aa8b7b0e6b078166bf4f5699f9441c654f4c6ac
Ajay V <ajayveee@gmail.com>
2015-10-02 00:09:31 -0400

    login to inbox

--- app/src/main/java/edu/uncc/amad/homework01/InboxActivity.java
+++ app/src/main/java/edu/uncc/amad/homework01/InboxActivity.java
@@ -1,31 +1,22 @@
 package edu.uncc.amad.homework01;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.parse.Parse;
-import com.parse.ParseUser;
-
-public class LoginActivity extends AppCompatActivity {
+public class InboxActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        if(ParseUser.getCurrentUser() != null){
-            Intent inbox = new Intent(this, InboxActivity.class);
-            startActivity(inbox);
-            finish();
-        }
+        setContentView(R.layout.activity_inbox);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_inbox, menu);
         return true;
     }
 
